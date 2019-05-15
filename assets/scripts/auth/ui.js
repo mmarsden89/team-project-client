1cbda49105057dc1292bde09f5cb2e90a4c24806
mmarsden89 <mmarsden89@gmail.com>
2019-05-15 11:45:20 -0400

    Add updates to authorization forms
    - Updated switch between nav views on login, logout, changepass

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,7 +3,8 @@ const store = require('./../store.js')
 const signUpSuccess = function (data) {
   console.log('success')
   $('form').trigger('reset')
-  $('#sign-up-form').hide()
+  $('#sign-up-form').fadeOut(500)
+  setTimeout(signInFade, 520)
 }
 
 const signUpFailure = function (data) {
@@ -15,7 +16,12 @@ const signInSuccess = function (data) {
   console.log('success')
   store.user = data.user
   $('form').trigger('reset')
-  $('#login-form').hide()
+  $('#login-form').fadeOut(500)
+  setTimeout(showAccountButtons, 525)
+}
+
+const showAccountButtons = function () {
+  $('#account-buttons').fadeIn(500)
 }
 
 const signInFailure = function (data) {
@@ -26,6 +32,8 @@ const signInFailure = function (data) {
 const changePasswordSuccess = function (data) {
   console.log('success')
   $('form').trigger('reset')
+  $('#change-password').fadeOut(500)
+  setTimeout(showAccountButtons, 520)
 }
 
 const changePasswordFailure = function (data) {
@@ -34,13 +42,37 @@ const changePasswordFailure = function (data) {
 }
 
 const signOutSuccess = function () {
-  console.log('success')
-  store.user = null
+  console.log('success of log out')
+  store.user.token = null
+  $('#account-buttons').fadeOut(500)
+  setTimeout(signInFade, 520)
+}
+
+const signInFade = function () {
+  $('#login-form').fadeIn(500)
 }
 
 const signOutFailure = function () {
   console.log('failure')
   $('form').trigger('reset')
+}
+
+const signUpFormFadeIn = function () {
+  $('#sign-up-form').fadeIn(500)
+}
+
+const loginToSignup = function () {
+  $('#login-form').fadeOut(500)
+  setTimeout(signUpFormFadeIn, 500)
+}
+
+const toChangePassword = function () {
+  $('#account-buttons').fadeOut(500)
+  setTimeout(changePassShow, 520)
+}
+
+const changePassShow = function () {
+  $('#change-password').fadeIn(500)
 }
 
 module.exports = {
@@ -51,5 +83,7 @@ module.exports = {
   changePasswordSuccess,
   changePasswordFailure,
   signOutSuccess,
-  signOutFailure
+  signOutFailure,
+  loginToSignup,
+  toChangePassword
 }
