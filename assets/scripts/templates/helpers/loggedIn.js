fbcd59fa89ddb96f6908564a6ba1dd80bc2af03d
tsosvielle <trevor.sosvielle@gmail.com>
2019-05-16 11:28:04 -0400

    Logs clear except handlebars logs!
    modified:   assets/scripts/auth/ui.js
    modified:   assets/scripts/blogs/events.js
    modified:   assets/scripts/blogs/ui.js
    modified:   assets/scripts/comments/ui.js
    modified:   assets/scripts/templates/helpers/blogOwned.js
    modified:   assets/scripts/templates/helpers/commentOwned.js
    modified:   assets/scripts/templates/helpers/loggedIn.js

--- assets/scripts/templates/helpers/loggedIn.js
+++ assets/scripts/templates/helpers/loggedIn.js
@@ -3,10 +3,8 @@ const store = require('../../store')
 
 const loggedIn = function () {
   if (store.user !== null) {
-    console.log('true')
     return true
   } else {
-    console.log('false')
     return false
   }
 }
